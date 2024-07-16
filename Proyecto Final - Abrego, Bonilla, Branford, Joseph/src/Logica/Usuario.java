8c380d2ce815e5decc2d84c217fac3047d1b03d9
Nathaly Bonilla <127335373+NathalyB-Mcklean@users.noreply.github.com>
2024-07-15 19:58:54 -0500

    Prueba 2

--- Proyecto Final - Abrego, Bonilla, Branford, Joseph/src/Logica/Usuario.java
+++ Proyecto Final - Abrego, Bonilla, Branford, Joseph/src/Logica/Usuario.java
@@ -8,7 +8,7 @@ public class Usuario {
     // Arreglo estático para almacenar todos los usuarios
     private static Usuario[] usuarios = new Usuario[10];
     private static int contadorUsuarios = 0;
-
+//arreglo
     public Usuario(String nombre, String correo, String contraseña) {
         this.nombre = nombre;
         this.correo = correo;
